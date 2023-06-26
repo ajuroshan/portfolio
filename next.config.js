03ae288a9653379494cc7a79628887e00f13b434
ajuroshan <ajuaju0483@gmail.com>
2023-06-26 19:55:10 +0530

    fixed module exports

--- next.config.js
+++ next.config.js
@@ -3,7 +3,5 @@
 module.exports = {
 	// Add the following configuration to enable static HTML export
 	// and specify the output directory
-	output: {
-		publicFolder: "/public", // Change '/public' to the desired output directory
-	},
+	output: "/public", // Change '/public' to the desired output directory
 };
