55023ef92ecf09de16428c4e188d9d7e5e867b69
Aju Roshan <70354172+ajuroshan@users.noreply.github.com>
2024-12-15 00:26:03 +0530

    undo clarity

--- app/layout.jsx
+++ app/layout.jsx
@@ -34,12 +34,6 @@ export default function RootLayout({ children }) {
 				<meta property="og:url" content="https://www.ajuroshan.me" />
 				<link rel="icon" type="image/svg+xml" sizes="any" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🤗</text></svg>" />
 				
-				{(function(c,l,a,r,i,t,y){
-        				c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        				t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        				y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    					})(window, document, "clarity", "script", "pdz9c7fkf6");}
-				
 			</head>
 
 			<body className={clsx("bg-[#0d0d0d] text-[#949494] ", montserrat.className)}>
